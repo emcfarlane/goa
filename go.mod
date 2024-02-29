eaa97179549aec8743c4fbbed1686f8d937b6498
Edward McFarlane <emcfarlane@buf.build>
2024-02-29 12:46:30 -0500

    init2

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/emcfarlane/goa
 
 go 1.22.0
+
+require github.com/srikrsna-buf/goa v0.0.0-20240229174231-798a7dd3b2ae
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package goa
 
-import ()
+import (
+	"github.com/srikrsna-buf/goa/pkg1"
+)
 
 type GoA struct{}
 
 func main() {
 	// Your code here
+	_ = pkg1.Symbol{}
 }
